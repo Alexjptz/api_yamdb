9a06513c92e90e7b9044fecfa92420e4f5404c4a
Alexander Golenev <algolenev@yandex.ru>
2020-12-13 19:20:37 +0300

    Start Point

---
+++ api_v1/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Categories(models.Model):
+    pass
+
+
+class Genres(models.Model):
+    pass
+
+
+class Titels(models.Model):
+    pass
+
+
+class Rewiews(models.Model):
+    pass
+
+
+class Comments(models.Model):
+    pass
