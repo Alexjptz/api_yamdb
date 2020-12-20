004de881960d2270a0eb34570bc111c6ce23eb8d
Aron <aron@Mac.local>
2020-12-20 19:36:48 +0300

    aron

--- api_v1/models.py
+++ api_v1/models.py
@@ -1,19 +1,72 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models.deletion import SET_NULL
+from django.db.models.fields import IntegerField
+
+from django.utils.text import slugify
 
 User = get_user_model()
 
 
+def get_unique_slug(self, model):
+    slug = slugify(self.name)
+    unique_slug = slug
+    num = 1
+    while model.objects.filter(slug=unique_slug).exists():
+        unique_slug = '{}-{}'.format(slug, num)
+        num += 1
+    return unique_slug
+
+
 class Categories(models.Model):
-    pass
+    name = models.CharField('Название категории', max_length=50)
+    slug = models.SlugField(
+        'URL путь', unique=True, max_length=50, blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = get_unique_slug(self, Categories)
+        super().save(*args, **kwargs)
 
 
 class Genres(models.Model):
-    pass
+    name = models.CharField('Название жанра', max_length=50)
+    slug = models.SlugField(
+        'URL путь', unique=True, max_length=99, blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = get_unique_slug(self, Genres)
+        super().save(*args, **kwargs)
 
 
 class Titels(models.Model):
-    pass
+    name = models.CharField(max_length=50, blank=False, null=False)
+    year = models.PositiveSmallIntegerField(
+        blank=True, null=True, db_index=True
+    )
+    rating = models.IntegerField()                    # тут непонятно как сделать
+    description = models.TextField(blank=True, null=True)
+    genre = models.ManyToManyField(Genres, db_index=True)
+    category = models.ForeignKey(
+        Categories,
+        related_name='titles',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        db_index=True
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Rewiews(models.Model):
--- api_v1/views.py
+++ api_v1/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import filters, viewsets, permissions
 
-# Create your views here.
+from .permissions import IsAdminOrSafeMethod
+from .models import Titels, Categories, Genres
+from .serializers import (
+    TitleSerializerRead, 
+    TitleSerializerWrite,
+    GenreSerializer,
+    CategorySerializer
+)
+
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminOrSafeMethod]
+    serializer_class = CategorySerializer
+    queryset = Categories.objects.all()
+    http_method_names = ['get', 'post', 'delete']
+    lookup_field = 'slug'
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['=name']
+    
+        
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminOrSafeMethod]
+    serializer_class = GenreSerializer
+    queryset = Genres.objects.all()
+    http_method_names = ['get', 'post', 'delete']
+    lookup_field = 'slug'
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['=name']
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Titels.objects.all()
+    permission_classes = [IsAdminOrSafeMethod]
+    filter_backends = (filters.SearchFilter)
+    search_fields = ['=name', '=year', '=category__slug', '=genre__slug']    
+
+    def get_serializer_class(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return TitleSerializerRead
+        return TitleSerializerWrite
